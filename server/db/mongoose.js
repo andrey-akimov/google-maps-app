ef431791bd15bc296ed09ad42dea753e6ba6103e
Andrey Akimov <rojer9999@gmail.com>
2018-03-31 19:31:31 +0300

    cloud mongoDB added

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://localhost/markers');
+mongoose.connect('mongodb://admin:admin@ds129939.mlab.com:29939/google-map-users');
 
 module.exports = mongoose;
